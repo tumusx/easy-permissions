9f7f53ceb8489d1a13b0f65fce81dc1fd6a5faac
murillo <tumusx1@gmail.com>
2023-05-01 20:09:13 -0300

    Finally read/insert values in datastore

--- app/src/main/java/com/github/tumusx/facilityapplication/MainActivity.kt
+++ app/src/main/java/com/github/tumusx/facilityapplication/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.github.tumusx.facilityapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.github.tumusx.easy_permissions.config.data.EasyPreferences
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private val easyPreferences = EasyPreferences(this@MainActivity)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        lifecycleScope.launch {
+            easyPreferences.insertValuesPreferences("FIRST_TEST", "FIRST TEST")
+            val value = easyPreferences.readValuePreferences("FIRST_TEST", String::class)
+            value.collect { testValue ->
+                Log.d("FIRST_TEST", testValue as String)
+            }
+        }
     }
 }
--- easy-permissions/src/main/java/com/github/tumusx/easy_permissions/config/data/EasyPreferences.kt
+++ easy-permissions/src/main/java/com/github/tumusx/easy_permissions/config/data/EasyPreferences.kt
@@ -4,31 +4,34 @@ import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.*
 import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.withContext
 
 const val EASY_PREFERENCES = "easy_preferences"
 
 val Context.dataStoreConfigure: DataStore<Preferences> by preferencesDataStore(name = EASY_PREFERENCES)
 
 class EasyPreferences(private val context: Context) {
-    private fun <T> getPreferencesKey(type: T, key: String) = when (type) {
+    private fun getPreferencesKey(type: Any, key: String) = when (type) {
         is String -> stringPreferencesKey(key)
         is Int -> intPreferencesKey(key)
         is Float -> floatPreferencesKey(key)
         is Double -> doublePreferencesKey(key)
         is Long -> longPreferencesKey(key)
-        is Byte -> byteArrayPreferencesKey(key)
         else -> stringPreferencesKey(key)
     }
 
-    suspend fun <T> readValuePreferences(key: String, type: T): Flow<Any?> {
-        return context.dataStoreConfigure.data.map { preferences ->
-            preferences[getPreferencesKey(type, key)]
+    suspend fun readValuePreferences(key: String, type: Any): Flow<Any?> {
+        return withContext(Dispatchers.Main) {
+            context.dataStoreConfigure.data.map { preferences ->
+                preferences[getPreferencesKey(type, key)]
+            }
         }
     }
 
-    suspend fun insertValuesPreferences(key: String, value: Any) =
+    suspend fun insertValuesPreferences(key: String, value: Any) {
         context.dataStoreConfigure.edit { settings ->
             when (value) {
                 is String -> settings[stringPreferencesKey(key)] = value
@@ -38,5 +41,5 @@ class EasyPreferences(private val context: Context) {
                 is Double -> settings[doublePreferencesKey(key)] = value
             }
         }
-
+    }
 }
