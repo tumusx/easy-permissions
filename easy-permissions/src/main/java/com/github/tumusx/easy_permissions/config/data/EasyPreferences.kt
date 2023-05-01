87b2d526d13f468fc1df48d98d80ebf472506607
murillo <tumusx1@gmail.com>
2023-04-30 21:31:48 -0300

    feat: Create data layer project with preferences store

--- easy-permissions/src/main/java/com/github/tumusx/easy_permissions/config/data/EasyPreferences.kt
+++ easy-permissions/src/main/java/com/github/tumusx/easy_permissions/config/data/EasyPreferences.kt
@@ -1,7 +1,42 @@
 package com.github.tumusx.easy_permissions.config.data
 
 import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.*
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
-class EasyPreferences (private val context: Context){
+const val EASY_PREFERENCES = "easy_preferences"
+
+val Context.dataStoreConfigure: DataStore<Preferences> by preferencesDataStore(name = EASY_PREFERENCES)
+
+class EasyPreferences(private val context: Context) {
+    private fun <T> getPreferencesKey(type: T, key: String) = when (type) {
+        is String -> stringPreferencesKey(key)
+        is Int -> intPreferencesKey(key)
+        is Float -> floatPreferencesKey(key)
+        is Double -> doublePreferencesKey(key)
+        is Long -> longPreferencesKey(key)
+        is Byte -> byteArrayPreferencesKey(key)
+        else -> stringPreferencesKey(key)
+    }
+
+    suspend fun <T> readValuePreferences(key: String, type: T): Flow<Any?> {
+        return context.dataStoreConfigure.data.map { preferences ->
+            preferences[getPreferencesKey(type, key)]
+        }
+    }
+
+    suspend fun insertValuesPreferences(key: String, value: Any) =
+        context.dataStoreConfigure.edit { settings ->
+            when (value) {
+                is String -> settings[stringPreferencesKey(key)] = value
+                is Int -> settings[intPreferencesKey(key)] = value
+                is Boolean -> settings[booleanPreferencesKey(key)] = value
+                is Float -> settings[floatPreferencesKey(key)] = value
+                is Double -> settings[doublePreferencesKey(key)] = value
+            }
+        }
 
 }
--- easy-permissions/src/main/java/com/github/tumusx/easy_permissions/ui/modal.kt
+++ easy-permissions/src/main/java/com/github/tumusx/easy_permissions/ui/modal.kt
@@ -1,2 +1,20 @@
 package com.github.tumusx.easy_permissions.ui
 
+import android.graphics.Color
+import android.provider.CalendarContract.Colors
+import android.view.LayoutInflater
+import android.view.View
+import com.github.tumusx.easy_permissions.databinding.EasyPermissionsLayoutBinding
+
+fun View.showModalDescriptionPermission(
+    titleModal: String,
+    descriptionModal: String,
+    buttonText: String,
+    listener: () -> Unit
+) {
+    val modalView = EasyPermissionsLayoutBinding.inflate(LayoutInflater.from(context))
+    modalView.modalDescription.text = descriptionModal
+    modalView.modalTitle.text = titleModal
+    modalView.modalConfirmButton.text = buttonText
+
+}
